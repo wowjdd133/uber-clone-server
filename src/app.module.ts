947d6b2f54a53588570923fd8c10c1100d30f12b
wowjdd133 <wowjddl133@naver.com>
2025-03-19 20:49:15 +0900

    redis 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+    RedisModule.forRoot({
+      type: 'single',
+      options: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT || '6379'),
+        // password: process.env.REDIS_PASSWORD,
+      },
+    }),
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as session from 'express-session';
+import Redis from 'ioredis';
+import * as passport from 'passport';
+import { RedisStore } from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const redisClient = new Redis();
+  // redisClient.connect().catch(console.error);
+
+  const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'myapp:',
+  });
+
+  app.use(
+    session({
+      secret: configService.get<string>('SESSION_SECRET')!,
+      saveUninitialized: false,
+      resave: false,
+      store: redisStore,
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 30, // 30분 유지,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  await app.listen(configService.get<string>('PORT') ?? 3000);
+  console.log('Server is open on port: ', configService.get<string>('PORT'));
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
